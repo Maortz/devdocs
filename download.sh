ec1605eb3c087b1cd90b63f2c8025166d12c9384
maort <maort@nerfwall.com>
2023-02-07 19:51:47 +0200

    Better explain download.sh

--- download.sh
+++ download.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-# examples: python@3.10
-# run "bundle exec docs:list" for full downloadable
+# examples: ./download python@3.10
 
 PKG_NAME=$1
-PKG_NAME=${PKG_NAME//@/\~}
-URL="https://downloads.devdocs.io/${PKG_NAME}.tar.gz"
+PKG_URL_NAME=${PKG_NAME//@/\~}
+URL="https://downloads.devdocs.io/${PKG_URL_NAME}.tar.gz"
 
 
 echo "Download from:"
 echo $URL
 echo "and move to ~/Downloads. Then run:"
-echo "bundle exec thor docs:download python@3.10"
+echo "bundle exec thor docs:download ${PKG_NAME}"
 echo ""
 echo "For list of packages, run:"
 echo "bundle exec thor docs:list"
